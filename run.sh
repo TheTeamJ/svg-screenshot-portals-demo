7ab913631fedc06101e36e7d6899e5e64de15a70
daiiz <da.iz.mg@gmail.com>
2019-06-01 15:00:25 +0900

    update launch script

--- run.sh
+++ run.sh
@@ -1,2 +1,8 @@
+#!/usr/bin/env bash
+
 python -m http.server 8000 &
-python -m http.server 8080
+pid[0]=$!
+python -m http.server 8080 &
+pid[1]=$!
+trap "kill ${pid[0]} ${pid[1]}; exit 1" INT
+wait
